95b72909e0f543304ae3082c887bc428b3af809f
Hamza <hamzashah02@outlook.com>
2026-04-13 12:01:42 +0100

    abc

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="qasystem",
+    version="0.0.1",
+    author="Hamza",
+    author_email="hamzashah02@outlook.com",
+    packages=find_packages(),
+    install_requires=["langchain","langchainhub","bs4","tiktoken","openai","boto3==1.34.37","langchain_community","chromadb","awscli","streamlit","pypdf","faiss-cpu"]
+)
